3c338b9c4803f3ba1e2777cb66b170b1bb9aeccd
Adarsh Anand <adarsh.anand@intel.com>
2025-03-07 04:04:55 +0530

    User configurable max_new_tokens for generation (#38)
    Add user configurable `max_new_tokens` for generation. Fixes #6
    
    ---------
    
    Signed-off-by: harshit-parikh-28 <harshit.parikh@intel.com>
    Co-authored-by: harshit-parikh-28 <harshit.parikh@intel.com>

--- src/llmart/config.py
+++ src/llmart/config.py
@@ -448,7 +448,8 @@ class LLMartConf(CoreConf):
     early_stop: bool = True
     val_every: int = 50
     save_every: int = 50
-
+    max_new_tokens:int = 512
+    
     model: PipelineConf
     attack: AttackConf
     response: ResponseConf = field(default_factory=ResponseConf)

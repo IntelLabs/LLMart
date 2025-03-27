b446c9782b93589c641ec6c08562f8ef83b3f2db
Adarsh Anand <adarsh.anand@intel.com>
2025-03-28 01:00:48 +0530

    Added `find_executable_batch_size` (#42)
    Fixes #5
    
    ---------
    
    Co-authored-by: sushma-4 <sushma.yellapragada@intel.com>

--- src/llmart/__main__.py
+++ src/llmart/__main__.py
@@ -6,6 +6,7 @@
 
 import hydra
 from omegaconf import OmegaConf, DictConfig
+from accelerate import Accelerator, find_executable_batch_size
 
 from llmart import config
 from llmart import run_attack
@@ -14,9 +15,22 @@ from llmart import run_attack
 @hydra.main(version_base=None, config_name="llmart")
 def main(dict_config: DictConfig):
     cfg: config.LLMartConf = OmegaConf.to_object(dict_config)  # type: ignore
+    
+    if cfg.per_device_bs == "auto":
+        @find_executable_batch_size(starting_batch_size=128)
+        def loop_optimal_bs(batch_size):
+            cfg.per_device_bs = batch_size
+            cfg.validate_parameters()
+            run_attack(cfg)
+        return loop_optimal_bs()
 
-    run_attack(cfg)
+    elif cfg.per_device_bs >= 1:
+        cfg.validate_parameters()
+        run_attack(cfg)
 
+    else:
+        print("Please enter the per_device_bs parameter correctly; enter an integer or set it to auto")
+    
 
 if __name__ == "__main__":
     main()
